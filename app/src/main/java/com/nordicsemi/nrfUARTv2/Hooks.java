ec7b43448f4223eba37a9d3b242a1f265fba6c0d
Ale Battisti <alebat25@yahoo.it>
2018-02-12 16:15:57 +0100

    Test me

--- app/src/main/java/com/nordicsemi/nrfUARTv2/Hooks.java
+++ app/src/main/java/com/nordicsemi/nrfUARTv2/Hooks.java
@@ -6,55 +6,23 @@ import android.util.Log;
 
 public class Hooks {
 
-    private final static String ACQUIRE = "R";
-    private final static String INFO = "?";
-    private final static int LENGTH_WHITE = 256;
-    private final static int LENGTH_IR = 288;
+    private final static String ACQUIRE = "MISURA SINGOLA";
+    private final static String ACQUIRE_LOOP = "MISURA CONTINUA";
     @SuppressLint("StaticFieldLeak")
     private static MainActivity activity;
     private static Runnable onDisconnect;
-    private static Rice onReceived;
-    private static Pro onProgress;
-    private static final boolean test = false;
-    private static StringBuilder result = null;
-    private static final int DIM_PACKETS = 5;
-    private static int received = 0;
+    private static Ricevuto onReceived;
+    private static final boolean test = true;
 
     public static void received(String text) {
-        if (text.startsWith("[")) {
-            if (result != null)
-                Log.e("WASTED", result.toString());
-            result = new StringBuilder();
-            result.append(text);
-            received = 0;
-        } else if (text.endsWith("]")) {
-            result.append(text);
-            complete(result.toString());
-            received = LENGTH_IR / DIM_PACKETS;
-        } else {
-            result.append(text);
-        }
-        received++;
-        onProgress.gresso(received, LENGTH_IR / DIM_PACKETS);
+        complete(text);
     }
 
     public static void complete(String text) {
         Log.i("TAG", "received " + text);
-        if (text.contains("[") && text.contains("]")) {
-            String[] t = text.substring(text.indexOf('[') + 3, text.indexOf(']')).split(" ");
-            int[] brusciutto = new int[512];
-            if (t.length < 512) {
-                for (int i = 0; i < t.length; i++) {
-                    brusciutto[i] = Integer.parseInt(t[i].toUpperCase(), 16);
-                }
-                char code = t.length == LENGTH_IR ? 'I' : t.length == LENGTH_WHITE ? 'W' : 'X';
-                if (onReceived != null) {
-                    onReceived.vuto(text, code, brusciutto, t.length);
-                }
-            } else
-                Log.w("TAG", "Merda!!");
-        } else
-            Log.i("TAG", "Merda!");
+        if (onReceived != null) {
+            onReceived.ricevuto(text, text.length());
+        }
     }
 
     public static void connected(final MainActivity activity) {
@@ -72,13 +40,14 @@ public class Hooks {
             if (onDisconnect != null)
                 onDisconnect.run();
         } else
-            Log.wtf("TAG", "Merda!!!");
+            Log.wtf("TAG", "Problema!!! 253");
     }
 
-    public static void acquire() {
+    public static void acquire(boolean loop) {
         if (activity != null) {
-            if (!test)
-                activity.send(ACQUIRE);
+            if (!test) {
+                activity.send(loop ? ACQUIRE_LOOP : ACQUIRE);
+            }
             else
                 new Thread(new Runnable() {
                     @Override
@@ -89,7 +58,7 @@ public class Hooks {
                             e.printStackTrace();
                         }
                         Intent i = new Intent(UartService.ACTION_DATA_AVAILABLE);
-                        i.putExtra(UartService.EXTRA_DATA, "[bc f2 e5 46 ae 89 e7 7d 53 bc 35 fd 78 ab 5f e8 54 cf 6b 23 fd 46 ef 8e a8 e5 f6 df ca dc 73 c4 bc f2 e5 46 ae 89 e7 7d 53 bc 35 fd 78 ab 5f e8 54 cf 6b 23 fd 46 ef 8e a8 e5 f6 df ca dc 73 c4]".getBytes());
+                        i.putExtra(UartService.EXTRA_DATA, "Rlevazione test,\n1234567890123".getBytes());
                         activity.UARTStatusChangeReceiver.onReceive(activity, i);
                     }
                 }).start();
@@ -104,19 +73,11 @@ public class Hooks {
         Hooks.onDisconnect = callback;
     }
 
-    public static void setOnReceived(Rice callback) {
+    public static void setOnReceived(Ricevuto callback) {
         Hooks.onReceived = callback;
     }
 
-    public static void setOnProgress(Pro callback) {
-        Hooks.onProgress = callback;
-    }
-
-    interface Rice {
-        void vuto(String text, char code, int[] valori, int length);
-    }
-
-    interface Pro {
-        void gresso(int num, int outof);
+    interface Ricevuto {
+        void ricevuto(String text, int length);
     }
 }
--- app/src/main/java/com/nordicsemi/nrfUARTv2/ShowActivity.java
+++ app/src/main/java/com/nordicsemi/nrfUARTv2/ShowActivity.java
@@ -7,12 +7,9 @@ import android.support.v4.app.NavUtils;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.EditText;
-import android.widget.ProgressBar;
-
-import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.series.DataPoint;
-import com.jjoe64.graphview.series.LineGraphSeries;
+import android.widget.ToggleButton;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -21,7 +18,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-public class ShowActivity extends Activity implements Hooks.Rice, Hooks.Pro {
+public class ShowActivity extends Activity implements Hooks.Ricevuto {
 
     private boolean on;
 
@@ -30,10 +27,18 @@ public class ShowActivity extends Activity implements Hooks.Rice, Hooks.Pro {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show);
 
-        findViewById(R.id.graph).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.acqloop).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Hooks.acquire();
+                Hooks.acquire(false);
+            }
+        });
+
+        ((ToggleButton)findViewById(R.id.acqloop)).setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                Hooks.acquire(b);
             }
         });
 
@@ -51,7 +56,6 @@ public class ShowActivity extends Activity implements Hooks.Rice, Hooks.Pro {
         });
 
         Hooks.setOnReceived(this);
-        Hooks.setOnProgress(this);
     }
 
     @Override
@@ -67,58 +71,28 @@ public class ShowActivity extends Activity implements Hooks.Rice, Hooks.Pro {
     }
 
     @Override
-    public void vuto(String text, final char code, final int[] valori, final int length) {
-        float maxy = 251;
-        DataPoint[] points = new DataPoint[length];
-        for (int i = 0; i < length; i++) {
-            if (valori[i] > maxy)
-                maxy = valori[i];
-            points[i] = new DataPoint(i, valori[i]);
-            Log.v("TAG", "point" + valori[i]);
-        }
-
-        GraphView graph = (GraphView) findViewById(R.id.graph);
-
-        //Set manually the bounds because auto range is not so accurate
-        graph.getViewport().setMinX(0);
-        graph.getViewport().setMaxX(length);
-        graph.getViewport().setMinY(0);
-        graph.getViewport().setMaxY(maxy+5);
-        graph.getViewport().setYAxisBoundsManual(true);
-        graph.getViewport().setXAxisBoundsManual(true);
-        graph.getGridLabelRenderer().setGridColor(0xAAAAAAAA);
-        graph.getGridLabelRenderer().setNumHorizontalLabels(20);
-        graph.getGridLabelRenderer().setNumVerticalLabels(20);
-
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(points);
-        series.setThickness(2);
-        graph.removeAllSeries();
-        graph.addSeries(series);
-
+    public void ricevuto(final String text, final int length) {
         findViewById(R.id.textid).setBackgroundColor(0xFFFFFFFF);
         new Thread(new Runnable() {
             @Override
             public void run() {
-                StringBuilder b = new StringBuilder("power\n");
-                for (int i = 0; i < length; i++)
-                    b.append(valori[i]).append("\n");
                 try {
-                    File folder = new File(Environment.getExternalStorageDirectory(), "PhysioREC/Spectrum/");
+                    File folder = new File(Environment.getExternalStorageDirectory(), "PhysioREC/");
                     //noinspection ResultOfMethodCallIgnored
                     folder.mkdirs();
                     String id = new SimpleDateFormat("yyyyMMdd", Locale.ITALY).format(new Date()).toString() + '_';
                     id += ((EditText) findViewById(R.id.textid)).getText().toString();
-                    File f;
-                    int progressive = 0;
-                    do {
-                        f = new File(folder, String.format(Locale.ENGLISH, "%s_%c_%05d.csv", id, code, progressive++));
-                    } while (f.exists());
+                    File f = new File(folder, String.format(Locale.ENGLISH, "%s_rilevazioni.csv", id));
                     FileOutputStream y = new FileOutputStream(f, true);
-                    y.write(b.toString().getBytes());
+                    String s = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US).format(new Date()) +
+                            ", " +
+                            text.replace(",", "\\s") +
+                            "\n";
+                    y.write(s.getBytes());
                     y.close();
                 } catch (IOException e) {
                     e.printStackTrace();
-                    Log.wtf("TAG", "Merda!!!");
+                    Log.wtf("TAG", "Problema!!! 445");
                     findViewById(R.id.textid).setBackgroundColor(0xFFFF0000);
                 }
 
@@ -135,12 +109,5 @@ public class ShowActivity extends Activity implements Hooks.Rice, Hooks.Pro {
                 return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void gresso(int num, int outof) {
-        Log.i("Progress", "num: " + num + ", outof: " + outof);
-        ((ProgressBar)findViewById(R.id.progress_horizontal)).setProgress(num);
-        ((ProgressBar)findViewById(R.id.progress_horizontal)).setMax(outof);
     }
 }
